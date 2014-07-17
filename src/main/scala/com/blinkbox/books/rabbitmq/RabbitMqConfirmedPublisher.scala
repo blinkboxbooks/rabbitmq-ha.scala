cdfc279aa9d51e46f7b3300aa14a39401e49fb32
Jan Stette <jans@blinkbox.com>
2014-07-17 14:52:35 +0100

    Changed publisher actors to use a RabbitMQ channel per message, to make confirmations work reliably.

--- src/main/scala/com/blinkbox/books/rabbitmq/RabbitMqConfirmedPublisher.scala
+++ src/main/scala/com/blinkbox/books/rabbitmq/RabbitMqConfirmedPublisher.scala
@@ -1,7 +1,7 @@
 package com.blinkbox.books.rabbitmq
 
 import akka.actor.{ Actor, ActorLogging, ActorRef, Props }
-import akka.actor.Status.{ Status, Success, Failure }
+import akka.actor.Status.{ Success, Failure }
 import com.blinkbox.books.messaging.{ ContentType, Event }
 import com.rabbitmq.client._
 import com.rabbitmq.client.AMQP.BasicProperties
@@ -32,94 +32,74 @@ import RabbitMqConfirmedPublisher._
  * can be re-processed at its origin, e.g. when the upstream message is already coming from a persistent queue,
  * or other persistent storage.
  *
- * @param channel This is the RabbitMQ channel that messages will be published on. Normally, you should
- * create a dedicated Channel for each instance of this actor.
+ * @param connection This is the RabbitMQ connection that messages will be published on. The actor
+ * will be publishing messages concurrently on multiple threads, hence will create Channels to publish
+ * the messages on itself (publisher confirms don't work correctly if multiple threads publish to a single Channel,
+ * see the JavaDoc for the Channel class).
  *
- * @param exchange The name of the exchange to publish messages to. If set to None, this actor will publish
- * messages to the "default exchange", which in RabbitMQ means publishing directly to a queue, with the routing key
- * being the name of the queue.
- *
- * @param messageTimeout The timeout for each published message, i.e. the time at which the client will receive
- * a failure notification for a message if confirmation hasn't been received for this.
+ * @param config Settings that describe what how messages are published. Any exchange or queue specified here
+ * will be declared by this actor on startup.
  *
  */
-class RabbitMqConfirmedPublisher(channel: Channel, config: PublisherConfiguration)
+class RabbitMqConfirmedPublisher(connection: Connection, config: PublisherConfiguration)
   extends Actor with ActorLogging {
 
   import context.dispatcher
 
   private val exchangeName = config.exchange getOrElse ""
 
-  // Tracks sequence numbers of messages that haven't been confirmed yet, and who to tell about the result.
-  private[rabbitmq] var pendingMessages = Map[Long, ActorRef]()
-
-  // Enable RabbitMQ Publisher Confirms.
-  channel.confirmSelect()
-
-  // Callback for publisher confirm events.
-  channel.addConfirmListener(new ConfirmListener {
-    override def handleAck(seqNo: Long, multiple: Boolean) {
-      self ! Ack(seqNo, multiple)
-    }
-    override def handleNack(seqNo: Long, multiple: Boolean) {
-      self ! Nack(seqNo, multiple)
-    }
-  })
-
-  // Either declare exchange or queue, depending on what we're publishing to.
-  config.exchange match {
-    case Some(name) =>
-      channel.exchangeDeclare(name, "topic", true)
-      log.debug(s"Declared topic exchange $name, used as the exchange to publish to")
-    case None =>
-      channel.queueDeclare(config.routingKey, true, false, false, null)
-      log.debug(s"Declared queue ${config.routingKey}, used as the queue to publish directly to")
-  }
+  initConnection()
 
   override def receive = {
     case event: Event =>
-      val seqNo = channel.getNextPublishSeqNo
-      // Note: Can't name child actor based on seqNo alone, as getting the next seqNo and incrementing it
-      // is not an atomic operation.
+      log.debug(s"Received request to publish event, id=${event.header.id}")
       val singleMessagePublisher = context.actorOf(Props(
-        new SingleEventPublisher(channel, exchangeName, config.routingKey, seqNo)), name = s"msg-publisher-for-${event.header.id}")
-      singleMessagePublisher ! event
-      context.system.scheduler.scheduleOnce(config.messageTimeout, self, TimedOut(seqNo))
-      pendingMessages += seqNo -> sender
+        new SingleEventPublisher(createChannel(), exchangeName, config.routingKey, config.messageTimeout)),
+        name = s"msg-publisher-for-${event.header.id}")
+      singleMessagePublisher.tell(event, sender)
 
-    case FailedToPublish(seqNo, e) => updateConfirmedMessages(seqNo, false, publishFailure(e))
-    case Ack(seqNo, multiple) => updateConfirmedMessages(seqNo, multiple, Success())
-    case Nack(seqNo, multiple) => updateConfirmedMessages(seqNo, multiple, nackFailure)
-    case TimedOut(seqNo) => updateConfirmedMessages(seqNo, false, timeoutFailure)
     case msg => log.error(s"Unexpected message received: $msg")
   }
 
-  private val nackFailure = Failure(PublishException("Message not successfully received"))
-  private val timeoutFailure = Failure(PublishException(s"Message timed out after ${config.messageTimeout}"))
-  private def publishFailure(e: Throwable) = Failure(PublishException(s"Failed to publish message", e))
-
-  /**
-   * Find the messages affected by the ACK/NACK, send the response to them, and remove them from
-   * the collection of pending messages.
-   */
-  private def updateConfirmedMessages(seqNo: Long, multiple: Boolean, response: Status) {
-    val (confirmed, remaining) = pendingMessages.partition(isAffectedByConfirmation(seqNo, multiple))
-    if (confirmed.nonEmpty) {
-      log.debug(s"Received update for message $seqNo (multiple=$multiple): $response")
-    }
-    confirmed.foreach { case (_, originator) => originator ! response }
-    pendingMessages = remaining
+  /** Ensure the required queues, exchanges and bindings are present. */
+  private def initConnection() {
+    val channel = createChannel()
+    try
+      // Either declare exchange or queue, depending on what we're publishing to.
+      config.exchange match {
+        case Some(name) =>
+          channel.exchangeDeclare(name, "topic", true)
+          log.debug(s"Declared topic exchange $name, used as the exchange to publish to")
+        case None =>
+          channel.queueDeclare(config.routingKey, true, false, false, null)
+          log.debug(s"Declared queue ${config.routingKey}, used as the queue to publish directly to")
+      }
+    finally channel.close()
   }
 
-  /** Predicate for deciding whether a pending message is affected by a given ACK/NACK or not. */
-  private def isAffectedByConfirmation(seqNo: Long, multiple: Boolean): PartialFunction[(Long, ActorRef), Boolean] =
-    { case (messageSeqNo, _) => if (multiple) messageSeqNo <= seqNo else messageSeqNo == seqNo }
+  /** Create a Channel with Publisher Confirms enabled. */
+  private def createChannel(): Channel = {
+    val channel = connection.createChannel()
+    // Enable RabbitMQ Publisher Confirms.
+    channel.confirmSelect()
+    channel
+  }
 
 }
 
 object RabbitMqConfirmedPublisher {
 
-  /** Settings for publisher. */
+  /**
+   * Settings for publisher.
+   *
+   * @param exchange The name of the exchange to publish messages to. If set to None, this actor will publish
+   * messages to the "default exchange", which in RabbitMQ means publishing directly to a queue, with the routing key
+   * being the name of the queue.
+   *
+   * @param messageTimeout The timeout for each published message, i.e. the time at which the client will receive
+   * a failure notification for a message if confirmation hasn't been received for this.
+   *
+   */
   case class PublisherConfiguration(exchange: Option[String], routingKey: String, messageTimeout: FiniteDuration)
   object PublisherConfiguration {
     def apply(config: Config): PublisherConfiguration = {
@@ -130,39 +110,41 @@ object RabbitMqConfirmedPublisher {
     }
   }
 
-  /** Exception that may be returned in failure respones from actor. */
+  /** Exception that may be returned in failure responses from actor. */
   case class PublishException(message: String, cause: Throwable = null) extends IOException(message, cause)
-
-  private case class Ack(seqNo: Long, multiple: Boolean)
-  private case class Nack(seqNo: Long, multiple: Boolean)
-  private case class TimedOut(seqNo: Long)
-  private case class FailedToPublish(seqNo: Long, e: Throwable)
 
   /**
    * Helper class that's responsible for publishing a single event.
+   * Note that this performs blocking operations on the RabbitMQ API. This API is very hard to
+   * use in a synchronous non-blocking way, sadly, especially when using publisher confirms.
    */
-  private class SingleEventPublisher(channel: Channel, exchange: String, routingKey: String, seqNo: Long)
+  private class SingleEventPublisher(channel: Channel, exchange: String, routingKey: String, timeout: FiniteDuration)
     extends Actor with ActorLogging {
 
-    import context.dispatcher
-
     def receive = {
-      case event: Event => publishMessage(seqNo, event) match {
+      case event: Event => Try(publishMessage(event)) match {
         case util.Failure(e) =>
-          sender ! FailedToPublish(seqNo, e)
+          sender ! publishFailure(e)
           context.stop(self)
         case util.Success(_) =>
+          sender ! Success(())
           context.stop(self)
       }
       case msg => log.error(s"Unexpected message: $msg")
     }
 
-    // Lyra can make the basicPublish() method blocking (e.g. when the broker connection is down),
-    // hence the need to have a separate actor deal with each call, and the use of blocking(). 
-    private def publishMessage(seqNo: Long, event: Event) = Try {
-      blocking(channel.basicPublish(exchange, routingKey, propertiesForEvent(event), event.body.content))
-      log.debug(s"Published message $seqNo with routing key $routingKey")
+    /** The actor is given its own Channel so it's essential we close this when we're done. */
+    override def postStop() = channel.close()
+
+    private def publishMessage(event: Event) = blocking {
+      // Note: Lyra can make the basicPublish() method blocking (e.g. when the broker connection is down),
+      // so this needs to be inside the blocking{} block.
+      channel.basicPublish(exchange, routingKey, propertiesForEvent(event), event.body.content)
+      channel.waitForConfirmsOrDie(timeout.toMillis)
+      log.debug(s"Published message with ID ${event.header.id} with routing key '$routingKey'")
     }
+
+    private def publishFailure(e: Throwable) = Failure(PublishException(s"Failed to publish message", e))
 
     /** Convert Event metadata to RabbitMQ message properties. */
     private def propertiesForEvent(event: Event): BasicProperties = {
@@ -177,7 +159,7 @@ object RabbitMqConfirmedPublisher {
       // Optional properties.
       val userIdHeader = event.header.userId map { userId => (RabbitMqConsumer.UserIdHeader -> userId) }
       val transactionIdHeader = event.header.transactionId.map { transactionId => (RabbitMqConsumer.TransactionIdHeader -> transactionId) }
-      
+
       val allHeaders: Map[String, Object] = List(userIdHeader, transactionIdHeader).flatten.toMap
       builder.headers(allHeaders.asJava)
 
--- src/test/scala/com/blinkbox/books/rabbitmq/RabbitMqConfirmedPublisherTest.scala
+++ src/test/scala/com/blinkbox/books/rabbitmq/RabbitMqConfirmedPublisherTest.scala
@@ -5,11 +5,11 @@ import akka.actor.Status.Success
 import akka.pattern.ask
 import akka.testkit.{ ImplicitSender, TestActorRef, TestEventListener, TestKit }
 import akka.util.Timeout
-import com.blinkbox.books.messaging.{ContentType, Event, EventHeader}
+import com.blinkbox.books.messaging.{ ContentType, Event, EventHeader }
 import com.rabbitmq.client.AMQP.BasicProperties
-import com.rabbitmq.client.{ Channel, ConfirmListener }
+import com.rabbitmq.client.{ Channel, Connection }
 import com.typesafe.config.ConfigFactory
-import java.util.concurrent.atomic.AtomicLong
+import java.util.concurrent.TimeoutException
 import org.junit.runner.RunWith
 import org.mockito.ArgumentCaptor
 import org.mockito.Matchers._
@@ -20,7 +20,6 @@ import org.scalatest.concurrent.AsyncAssertions
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.mock.MockitoSugar
 import scala.concurrent.duration._
-
 import RabbitMqConfirmedPublisherTest._
 
 @RunWith(classOf[JUnitRunner])
@@ -32,120 +31,16 @@ class RabbitMqConfirmedPublisherTest extends TestKit(ActorSystem("test-system", 
   val ExchangeName = "test.exchange"
   val Topic = "test.topic"
   val TestMessageTimeout = 10.seconds
-  implicit val TestActorTimeout = Timeout(10.seconds)
+  implicit val TestActorTimeout = Timeout(12.seconds)
 
   //
-  // Synchronous tests. These check the confirmation handling in the main actor,
-  // but not publishing (as this is handled by a created child actor, which
-  // won't behave synchronously).
+  // Async actor tests.
   //
 
-  test("Single acked message") {
-    // Initialise actor and related mocks.
-    val (actor, channel, confirmListener) = setupActor(Some(ExchangeName))
-
-    // Ask actor to publish message.
-    val response = actor ? event("test 1")
-
-    // Fake a response from the Channel.
-    confirmListener.handleAck(0, false)
-
-    val util.Success(_) = response.value.get
-    assert(actor.underlyingActor.pendingMessages.isEmpty)
-  }
-
-  test("Single acked message to default exchange") {
-    val (actor, channel, confirmListener) = setupActor(None)
-
-    val response = actor ? event("test 1")
-
-    confirmListener.handleAck(0, false)
-
-    val util.Success(_) = response.value.get
-    assert(actor.underlyingActor.pendingMessages.isEmpty)
-  }
-
-  test("Single nacked message") {
-    val (actor, channel, confirmListener) = setupActor(Some(ExchangeName))
-    val response = actor ? event("test 1")
-    confirmListener.handleNack(0, false)
-
-    val util.Failure(PublishException(message, _)) = response.value.get
-    assert(message.contains("not successfully received"))
-
-    assert(actor.underlyingActor.pendingMessages.isEmpty)
-  }
-
-  test("Single acked message while others remain") {
-    val (actor, channel, confirmListener) = setupActor(Some(ExchangeName))
-    actor ! event("test 1")
-    val response = actor ? event("test 2")
-    actor ! event("test 3")
-
-    // Only ACK the middle message.
-    confirmListener.handleAck(1, false)
-
-    val util.Success(_) = response.value.get
-
-    // Should leave other messages pending.
-    assert(actor.underlyingActor.pendingMessages.keySet == Set(0, 2))
-  }
-
-  test("Single nacked message while others remain") {
-    val (actor, channel, confirmListener) = setupActor(Some(ExchangeName))
-    actor ! event("test 1")
-    val response = actor ? event("test 2")
-    actor ! event("test 3")
-
-    // Only NACK the middle message.
-    confirmListener.handleNack(1, false)
-
-    val util.Failure(PublishException(reason, _)) = response.value.get
-
-    // Should leave other messages pending.
-    assert(actor.underlyingActor.pendingMessages.keySet == Set(0, 2))
-  }
-
-  test("Ack multiple messages") {
-    val (actor, channel, confirmListener) = setupActor(Some(ExchangeName))
-    val response1 = actor ? event("test 1")
-    val response2 = actor ? event("test 2")
-    actor ! event("test 3")
-
-    // ACK messages up to and including the second one.
-    confirmListener.handleAck(1, true)
-
-    val util.Success(_) = response1.value.get
-    val util.Success(_) = response2.value.get
-
-    // Should leave later message pending.
-    assert(actor.underlyingActor.pendingMessages.keySet == Set(2))
-  }
-
-  test("Ack for unknown message") {
-    val (actor, channel, confirmListener) = setupActor(Some(ExchangeName))
-    actor ! event("test 1")
-    actor ! event("test 2")
-    actor ! event("test 3")
-
-    // ACK messages up to and including the second one.
-    confirmListener.handleAck(42, false)
-
-    // Should leave all message pending.
-    assert(actor.underlyingActor.pendingMessages.keySet == Set(0, 1, 2))
-  }
-
-  //
-  // Async tests. These check publishing, i.e. the actions handled in created child actors.
-  //
-
-  test("Publish message to named exchange") {
-    val (concurrentActor, channel, confirmListener) = asyncActor(Some(ExchangeName))
+  test("Publish XML message to named exchange") {
+    val (concurrentActor, channel) = initActor(Some(ExchangeName))
 
     concurrentActor ! event("test 1")
-
-    // Fake a response from the Channel.
-    confirmListener.handleAck(0, false)
 
     within(1000.millis) {
       expectMsgType[Success]
@@ -154,12 +49,9 @@ class RabbitMqConfirmedPublisherTest extends TestKit(ActorSystem("test-system", 
   }
 
   test("Publish message to default exchange") {
-    val (concurrentActor, channel, confirmListener) = asyncActor(None)
+    val (concurrentActor, channel) = initActor(None)
 
     concurrentActor ! event("test 1")
-
-    // Fake a response from the Channel.
-    confirmListener.handleAck(0, false)
 
     within(1000.millis) {
       expectMsgType[Success]
@@ -169,82 +61,71 @@ class RabbitMqConfirmedPublisherTest extends TestKit(ActorSystem("test-system", 
   }
 
   test("Publish message with content type") {
-    val (concurrentActor, channel, confirmListener) = asyncActor(None)
+    val (concurrentActor, channel) = initActor(None)
     concurrentActor ! eventJson("test 1")
-
-    // Fake a response from the Channel.
-    confirmListener.handleAck(0, false)
 
     within(1000.millis) {
       expectMsgType[Success]
-      // Should publish on the RabbitMQ "default exchange", whose name is the empty string.
       val captor = ArgumentCaptor.forClass(classOf[BasicProperties])
-      verify(channel).basicPublish(matcherEq(""), matcherEq(Topic), captor.capture() , any[Array[Byte]])
-      val props  = captor.getValue
+      verify(channel).basicPublish(matcherEq(""), matcherEq(Topic), captor.capture(), any[Array[Byte]])
+      val props = captor.getValue
       assert(props.getContentType === ContentType.JsonContentType.mediaType)
     }
   }
 
   test("Message times out") {
-    // Use a real, concurrent actor for this test case, with a very short timeout.
-    val (concurrentActor, channel, confirmListener) = asyncActor(None, 100.millis)
+    val timeout = 123.millis
+    val (concurrentActor, channel) = initActor(None, timeout)
+
+    // Make publishing take some time.
+    val timeoutException = new TimeoutException("Test timeout")
+    doThrow(timeoutException).when(channel).waitForConfirmsOrDie(anyLong)
 
     concurrentActor ! event("test")
 
-    val response = expectMsgType[Status.Failure](1.second)
+    // Message should time out.
+    val response = expectMsgType[Status.Failure](500.millis)
     assert(response.cause.isInstanceOf[PublishException])
-
-    // ACKing after timeout should have no effect.
-    confirmListener.handleAck(1, false)
-
-    expectNoMsg(1.second)
+    assert(response.cause.getCause == timeoutException)
+    
+    // Check that the actor used the right timeout.
+    verify(channel).waitForConfirmsOrDie(timeout.toMillis)
   }
 
   private def event(tag: String): Event = Event.xml("<test/>", EventHeader("test"))
   private def eventJson(tag: String): Event = Event.json("{}", EventHeader("json"))
 
-  private def setupActor(exchangeName: Option[String]): (TestActorRef[RabbitMqConfirmedPublisher], Channel, ConfirmListener) = {
-    val channel = mockChannel()
-    val newActor = TestActorRef(new RabbitMqConfirmedPublisher(channel, PublisherConfiguration(exchangeName, Topic, TestMessageTimeout)))
-    (newActor, channel, confirmListener(channel))
-  }
+  private def initActor(exchangeName: Option[String], messageTimeout: FiniteDuration = 1000.millis): (ActorRef, Channel) = {
+    val (connection, channel) = mockConnection()
 
-  private def asyncActor(exchangeName: Option[String], messageTimeout: FiniteDuration = 1000.millis): (ActorRef, Channel, ConfirmListener) = {
-    val channel = mockChannel()
-
-    // Create a waiter so we can wait for the (async) initialisation of the actor.
+    // Create a waiter so we can wait for the initialisation of the actor, which may happen in another thread.
     val actorInitWaiter = new Waiter()
     doAnswer(() => { actorInitWaiter.dismiss() })
-      .when(channel).addConfirmListener(any[ConfirmListener])
+      .when(channel).close()
 
     // Create actor under test.
     val newActor = system.actorOf(
-      Props(new RabbitMqConfirmedPublisher(channel, PublisherConfiguration(exchangeName, Topic, messageTimeout))))
+      Props(new RabbitMqConfirmedPublisher(connection, PublisherConfiguration(exchangeName, Topic, messageTimeout))))
 
     // Wait for it to be initialised.
     within(1.seconds) {
       actorInitWaiter.await()
     }
 
-    (newActor, channel, confirmListener(channel))
+    // Reset the mock.
+    reset(channel)
+
+    (newActor, channel)
   }
 
-  /** Create a mock RabbitMQ Channel that gives out valid sequence numbers.  */
-  private def mockChannel() = {
+  /** Create a mock RabbitMQ Connection that gives out a mock Channel. */
+  private def mockConnection() = {
+    val connection = mock[Connection]
     val channel = mock[Channel]
-    val seqNo = new AtomicLong(0L)
-    doAnswer(() => { seqNo.getAndIncrement }).when(channel).getNextPublishSeqNo
-    channel
+    doReturn(channel).when(connection).createChannel()
+    (connection, channel)
   }
 
-  /** Get hold of the actor's registered ConfirmListener. */
-  private def confirmListener(channel: Channel) = {
-    val confirmListenerArgument = ArgumentCaptor.forClass(classOf[ConfirmListener])
-    verify(channel).addConfirmListener(confirmListenerArgument.capture())
-    val confirmListener = confirmListenerArgument.getValue
-    assert(confirmListenerArgument.getValue != null, "Actor should have registered a confirm listener")
-    confirmListener
-  }
 }
 
 object RabbitMqConfirmedPublisherTest {
